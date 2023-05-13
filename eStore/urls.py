d36bff884dc79d3fe9ab87c59c8ba0bdad879531
AldiyarIkhsan <usoul31@gmail.com>
2023-05-13 20:33:37 +0500

    added application orders

--- eStore/urls.py
+++ eStore/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
-    path('api/', include(('api.urls','api'), namespace='api')),
+    path('api/', include(('api.urls', 'api'), namespace='api')),
     path('api/', include('orders.urls')),
 ]
